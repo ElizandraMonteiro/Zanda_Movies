4d5440c3bcb82379aace9457473c058af392a08b
Elizandra Monteiro <monteiroelizandra2017@gmail.com>
2023-11-09 11:08:20 -0300

    Changes02

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,129 +1,120 @@
-import { useState, useEffect } from "react";
-import { FiArrowLeft, FiClock } from "react-icons/fi";
-import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 
-import { useAuth } from "../../hooks/auth";
+import { api } from '../../services/api';
+import { useAuth } from '../../hooks/auth';
 
-import { api } from "../../services/api";
-import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
+import  moment  from 'moment-timezone';
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
 
-import moment from "moment-timezone";
-
-import { Container } from "./styles";
-
+import { FiClock, FiArrowLeft } from 'react-icons/fi';
+import { Container, Content, Writer, Tags, ButtonDelete } from "./styles.js";
 import { Header } from "../../components/Header";
-import { Input } from "../../components/Input";
+
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 import { ButtonText } from "../../components/ButtonText";
 import { Rating } from "../../components/Rating";
-import { Tag } from "../../components/Tag";
-import { Button } from "../../components/Button";
 
-export function Details() {
-  const [data, setData] = useState({});
-  const [loading, setLoading] = useState(false);
+
+
+export function Details(){
+  const [ data, setData ] = useState({});
 
   const params = useParams();
 
   const { user } = useAuth();
-
-  const avatarURL = user.avatar
-    ? `${api.defaults.baseURL}/files/${user.avatar}`
-    : avatarPlaceholder;
-
-  const formattedDate = moment
-    .utc(data.updated_at)
-    .tz("America/Sao_Paulo")
-    .format("DD/MM/YYYY HH:mm:ss");
-
-  const navigate = useNavigate();
-
-  function handleBack() {
-    navigate(-1);
-  }
-
-  async function handleRemove() {
-    const confirm = window.confirm("Deseja realmente remover o filme?");
-
-    if (confirm) {
-      setLoading(true);
-
-      try {
-        await api.delete(`/notes/${params.id}`);
-        navigate(-1);
-      } catch (error) {
-        if (error.response) {
-          alert(error.response.data.message);
-        } else {
-          alert("Não foi possível remover o filme.");
-          console.log("Erro ao remover o filme:", error);
-        }
-      } finally {
-        setLoading(false);
-      }
-    }
-  }
+ 
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
   useEffect(() => {
-    async function fetchMovie() {
+    async function fetMovie(){
       const response = await api.get(`/notes/${params.id}`);
       setData(response.data);
+
     }
 
-    fetchMovie();
+    fetMovie();
   }, []);
 
-  return (
+  const momentDate = moment.utc(data.updated_at).tz('America/Sao_Paulo').format('DD/MM/YYYY HH:mm:ss');
+
+  const navigate = useNavigate();
+  
+  function handleBack() {
+		navigate(-1);
+	}
+
+  async function handleRemove(){
+    const confirm = window.confirm("Deseja realmente remover a nota?");
+
+    if(confirm){
+      await api.delete(`/notes/${params.id}`);
+      navigate("/");
+    }
+  }
+
+  return(
     <Container>
-      <Header>
-        <Input placeholder="Pesquisar pelo título" />
-      </Header>
 
-      {data && (
-        <main>
-          <header>
-            <ButtonText onClick={handleBack}>
-              <FiArrowLeft />
-              Voltar
-            </ButtonText>
+      <Header />
+      {     
+        data && 
+          <main>
+              <Content>
+                <div className="back">
+                  <FiArrowLeft />
+                  <ButtonText 
+                    onClick={handleBack} 
+                    className="back" 
+                    title="Voltar" 
+                  />
+                </div>  
 
-            <div>
-              <h1>{data.title}</h1>
-              <Rating grade={data.rating} isBigSize />
-            </div>
+                <div className="title">
+                  <h1>{data.title}</h1>
+                  <Rating note={data.rating} noteEmpty/>
+                </div>
 
-            <div className="movie-info">
-              <p>
-                <img src={avatarURL} alt={user.name} />
-                Por {user.name}
-              </p>
+                <Writer>
+                  <img 
+                    className="writer"
+                    src={avatarUrl}
+                    alt={user.name}
+                  />
 
-              <div>
-                <FiClock />
+                  <p>Por {user.name}</p>
+                  <FiClock/>
+                  <p>{momentDate}</p>
+                </Writer>
 
-                <p>{formattedDate}</p>
-              </div>
-            </div>
-          </header>
+                <p>
+                  {data.description}
+                </p>
 
-          {data.tags && (
-            <section>
-              {data.tags.map((tag) => (
-                <Tag key={String(tag.id)} title={tag.name} />
-              ))}
-            </section>
-          )}
+                { data.tags && 
+                  <Section>
+                    <Tags>
+                      {
+                        data.tags.map(tag => (
+                          <Tag 
+                            key={String(tag.id)}
+                            title={tag.name}
+                          />
+                        ))
+                      }
+                    </Tags>
+                  </Section>
+                }
 
-          <p>{data.description}</p>
+            <ButtonDelete onClick={handleRemove}>
+              Excluir filme
+            </ButtonDelete>
 
-          <div>
-            <Button
-              title="Excluir filme"
-              onClick={handleRemove}
-              loading={loading}
-            />
-          </div>
-        </main>
-      )}
-    </Container>
+              </Content>
+          </main>
+      }
+
+    </Container> 
   );
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -4,90 +4,181 @@ export const Container = styled.div`
   width: 100%;
   height: 100vh;
 
-  display: grid;
-  grid-template-rows: 11.6rem auto;
-  grid-template-areas:
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+
+  @media (min-width: 700px){
+  
+    display: grid;
+    grid-template-rows: 5rem auto;
+    grid-template-areas:
     "header"
     "content";
 
-  > main {
-    grid-area: content;
-    overflow-y: auto;
+    > main {
+      width: 100%;
+      max-width: 1137px;
 
-    width: 100%;
-    max-width: 113.7rem;
-    margin: 4rem auto;
+      margin: 40px auto;
+      
 
-    header {
+      grid-area: content;
+      overflow-y: auto;
+      padding: 3.4rem 1rem;
+
+      svg {
+        color: ${({ theme }) => theme.COLORS.PINK};
+        margin-right: 0.5rem;
+      }
+    }
+
+
+    > main::-webkit-scrollbar {
+      width: 8px;
+    
+    }
+    
+    > main::-webkit-scrollbar-thumb {
+      background-color: ${({ theme }) => theme.COLORS.PINK};
+      border-radius: 8px;
+    
+    }
+  }
+`;
+
+export const Content = styled.div`
+
+  display: flex;
+  flex-direction: column;
+
+  .back {
+    display: flex;
+    align-items: center;
+    color: ${({ theme }) => theme.COLORS.PINK};
+  }
+
+  .title {
+    margin-top: 2rem;
+    margin-bottom: 1rem;
+
+    display: flex;
+    align-items: center;
+    justify-self: center;
+    gap: 1rem;
+  }
+
+  .tags {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  > button:first-child {
+    align-self: start;
+  }
+
+  > h1 {
+    font-weight: 500;
+    padding-top: 2rem;
+  }
+
+  > p {
+    font-size: 16px;
+    margin-top: 1rem;
+    margin-bottom: 3rem;
+
+    text-align: justify;
+  }
+
+  @media (max-width: 924px){
+    display: flex;
+    align-items: center;
+
+    .back {
+      margin-top: 1rem;
+      gap: 0.2rem;
+      > svg {
+        margin-top: 1rem;
+      }
+    }
+
+    .title {
       display: flex;
       flex-direction: column;
 
-      margin-bottom: 4rem;
-
-      button {
-        background: none;
-        border: none;
-      }
-
-      div {
-        display: flex;
-        align-items: center;
-      }
-
-      .movie-info {
-        gap: 0.8rem;
-        margin-top: 2.4rem;
-
-        p:nth-child(1),
-        div {
-          display: flex;
-          align-items: center;
-          gap: 0.8rem;
-        }
-
-        img {
-          width: 1.6rem;
-          height: 1.6rem;
-          border-radius: 50%;
-        }
-
-        svg {
-          color: ${({ theme }) => theme.COLORS.PINK};
-        }
-      }
-
-      h1 {
+      > h1 {
+        font-size: 1.8rem;
         font-weight: 500;
-        font-size: 3.6rem;
-        margin-right: 1.9rem;
+        padding-top: 2rem;
       }
     }
 
-    > p {
-      text-align: justify;
-      color: ${({ theme }) => theme.COLORS.WHITE};
+  }
+`;
 
-      margin-top: 4rem;
-      max-width: calc(100% - 0.8rem);
+export const Writer = styled.div`
 
-      line-height: 2.1rem;
-    }
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 
-    button {
-      background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
-      color: ${({ theme }) => theme.COLORS.PINK};
+  margin-bottom: 1.2rem;
 
-      width: calc(50% - 2rem);
-      margin-top: 4rem;
-    }
+> .writer {
+    display: flex;
+    align-items: center;
+    
+    width: 1rem;
+    height: 1rem;
+
+    border-radius: 50%;
   }
 
-  > main::-webkit-scrollbar {
-    width: 0.8rem;
+  > img {
+    margin-right: 0.5rem;
   }
 
-  > main::-webkit-scrollbar-thumb {
-    background-color: ${({ theme }) => theme.COLORS.PINK};
-    border-radius: 0.8rem;
+  > svg {
+    color: ${({ theme }) => theme.COLORS.PINK};
+    font-size: 1.5rem;
+
+    margin-left: 1rem;
+    margin-right: 0.5rem;
+  }
+
+  @media (max-width: 700px){
+    .writer {
+      display: flex;
+      flex-direction: column;
+    }
+  }
+`;
+
+export const Tags = styled.div`
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+
+  display: flex;
+  gap: 0.5rem;
+`;
+
+export const ButtonDelete = styled.button`
+  width: 50%;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_1000};
+
+  color: ${({ theme }) => theme.COLORS.PINK};
+
+  height: 3rem;
+  border: 0;
+  padding: 0 1.6rem;
+  margin-top: 1.6rem;
+  border-radius: 10px;
+
+  font-weight: 500;
+
+  &:disabled {
+    opacity: 0.5;
   }
 `;
